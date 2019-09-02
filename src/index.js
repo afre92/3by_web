77e08ddad44fdb4002eb8369c0fdf24572f70b0b
Andres <afre92@gmail.com>
2019-09-02 19:54:31 -0400

    navbar and routes added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Greetings from './components/Greetings';
+import SignupPage from './components/signup/SignupPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render((
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+      <Route exact path="/" component={Greetings} />
+      <Route path="/signup" component={SignupPage} />
+    </div>
+  </Router>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
