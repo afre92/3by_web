564261c42f6f89222d82eb6feaa941d1ff73793f
Andres <afre92@gmail.com>
2019-09-02 20:31:18 -0400

    sign form added

---
+++ src/components/signup/SignupForm.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+
+export default class SignupForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      password_confirmation: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Join our community</h1>
+        <div className="form-group">
+          <label className="control-label">username</label>
+          <input
+            onChange={this.onChange} 
+            type="text"
+            name="username"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">email</label>
+          <input
+            onChange={this.onChange} 
+            type="text"
+            name="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">password</label>
+          <input
+            onChange={this.onChange} 
+            type="password"
+            name="password"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">password confirmation</label>
+          <input
+            onChange={this.onChange} 
+            type="password"
+            name="password_confirmation"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary btn-lg">
+            sign up
+          </button>
+        </div>
+
+      </form>
+    )
+  }
+}
