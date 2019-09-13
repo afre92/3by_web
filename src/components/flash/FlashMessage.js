372b2b65396382aa6a071afa860e3f25823497b9
Andres Rojas <afre92@gmail.com>
2019-09-13 15:11:15 -0400

    deleting flash messages reducer and action added and working

--- src/components/flash/FlashMessage.js
+++ src/components/flash/FlashMessage.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames';
 
 export default class FlashMessage extends Component {
+    constructor(props){
+        super(props)
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(){
+        this.props.deleteFlashMessage(this.props.message.id);
+    }
+
+
     render() {
         const { id, type, text } = this.props.message;
         return (
@@ -10,6 +20,7 @@ export default class FlashMessage extends Component {
                 'alert-success': type === 'success',
                 'alert-danger': type === 'error'
              })}>
+                 <button onClick={this.onClick} className="close"><span>&times;</span></button>
                 {text}
             </div>
         )
@@ -18,5 +29,6 @@ export default class FlashMessage extends Component {
 
 
 FlashMessage.propTypes = {
-    message: PropTypes.object.isRequired
+    message: PropTypes.object.isRequired,
+    deleteFlashMessage: PropTypes.func.isRequired
 }
--- src/components/flash/FlashMessagesList.js
+++ src/components/flash/FlashMessagesList.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import FlashMessage from './FlashMessage'
+import { deleteFlashMessage } from '../../actions/flashMessages'
 
 class FlashMessagesList extends Component {
     render() {
         const messages = this.props.messages.map( message =>
-            <FlashMessage key={message.id} message={message} />
+            <FlashMessage key={message.id} message={message} deleteFlashMessage={this.props.deleteFlashMessage}/>
         )
         return (
             <div>
@@ -17,7 +18,8 @@ class FlashMessagesList extends Component {
 }
 
 FlashMessagesList.propTypes = {
-    messages: PropTypes.array.isRequired
+    messages: PropTypes.array.isRequired,
+    deleteFlashMessage: PropTypes.func.isRequired
 }
 
 function mapStateToProps(state) {
@@ -26,4 +28,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(FlashMessagesList);
+export default connect(mapStateToProps, {deleteFlashMessage})(FlashMessagesList);
