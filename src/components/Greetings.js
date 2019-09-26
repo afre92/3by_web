86c3fd5c27e8c9ccdbce0afd26b51a003adc2fe1
Andres <afre92@gmail.com>
2019-09-25 20:02:40 -0400

    theme and assets added

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -5,6 +5,17 @@ class Greetings extends React.Component {
     return (
       <div className="jumbotron">
         <h1>Helooooooooo</h1>
+        <div class="row">
+            <div class="col-md-12">
+              <button class="btn">Default</button>
+              <button class="btn btn-primary">Primary</button>
+              <button class="btn btn-info">Info</button>
+              <button class="btn btn-success">Success</button>
+              <button class="btn btn-warning">Warning</button>
+              <button class="btn btn-danger">Danger</button>
+              <button class="btn btn-neutral">Neutral</button>
+            </div>
+          </div>
       </div>
       
     )
