fd0892075d9a6106d6206cad2b74ee3485642b21
Andres <afre92@gmail.com>
2019-10-04 16:08:26 -0400

    cards added to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,9 +3,75 @@ import React, { Component } from 'react'
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1>hello this is private</h1>
-      </div>
+      <div className="dashboard center-horizontally">
+        <div className="dashboard-tiles col-8 " >
+        <div className="flex-container">
+        <div class="card card-stats bg-danger col-md-4">
+              <div class="card-body">
+                <div class="justify-content-center">
+                  <div class="numbers">
+                    <p class="card-title">100%</p>
+                    <p class="card-category text-white">Safe</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card card-stats bg-danger col-md-4">
+              <div class="card-body">
+                <div class="justify-content-center">
+                  <div class="numbers">
+                    <p class="card-title">100%</p>
+                    <p class="card-category text-white">Safe</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card card-stats bg-danger col-md-4">
+              <div class="card-body">
+                <div class="justify-content-center">
+                  <div class="numbers">
+                    <p class="card-title">100%</p>
+                    <p class="card-category text-white">Safe</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex-container">
+        <div class="card card-stats bg-danger col-md-4">
+              <div class="card-body">
+                <div class="justify-content-center">
+                  <div class="numbers">
+                    <p class="card-title">100%</p>
+                    <p class="card-category text-white">Safe</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card card-stats bg-danger col-md-4">
+              <div class="card-body">
+                <div class="justify-content-center">
+                  <div class="numbers">
+                    <p class="card-title">100%</p>
+                    <p class="card-category text-white">Safe</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card card-stats bg-danger col-md-4">
+              <div class="card-body">
+                <div class="justify-content-center">
+                  <div class="numbers">
+                    <p class="card-title">100%</p>
+                    <p class="card-category text-white">Safe</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
     )
   }
 }
