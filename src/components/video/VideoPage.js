aaec5e16be756701e45f50068575e813ccbc7c02
Andres Rojas <afre92@gmail.com>
2019-10-16 22:09:24 -0400

    wip in videopage

--- src/components/video/VideoPage.js
+++ src/components/video/VideoPage.js
@@ -6,7 +6,11 @@ export default class VideoPage extends Component {
       <div class="profile-page">
         <div class="wrapper mb-5 container">
           <div class="page-header">
-            <h3>helloet ehre</h3>
+          <h3>helloet ehre</h3>
+              <div className="video-page center-horizontally">
+                
+                <iframe width="1008" height="567" src="https://www.youtube.com/embed/Ghywp7i032A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
           </div>
         </div>
       </div>
