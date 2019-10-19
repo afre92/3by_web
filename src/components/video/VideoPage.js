041b5b174c78fa0c5e5432dc69c0738ddd6f5588
Andres <afre92@gmail.com>
2019-10-18 23:36:02 -0400

    reaction emojis added

--- src/components/video/VideoPage.js
+++ src/components/video/VideoPage.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react'
 export default class VideoPage extends Component {
   render() {
     return (
-      <div class="profile-page">
+      <div class="section profile-page">
         <div class="wrapper mb-5 container">
           <div class="page-header">
-          <h3>helloet ehre</h3>
+            <h3 className="text-center">helloet ehre</h3>
               <div className="video-page center-horizontally">
                 
                 <iframe width="1008" height="567" id="ifrm" src="https://www.youtube.com/embed/Ghywp7i032A?rel=0&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
               </div>
+              <div className="video-reactions display-inline center-horizontally section">
+                  <img src="assets/img/angry.png" alt="Rounded" className="img-fluid rounded shadow reaction-emoji" />
+                  <img src="assets/img/in-love.png" alt="Rounded" className="img-fluid rounded shadow reaction-emoji" />
+                </div>
           </div>
         </div>
       </div>
