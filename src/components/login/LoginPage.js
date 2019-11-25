b3f97c3a5f78bdcccde9b6f8cb2ae4c96db09ffb
Andres <afre92@gmail.com>
2019-11-24 23:43:37 -0500

    navigation issue in login page solved

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -6,7 +6,7 @@ export default class LoginPage extends Component {
     return (
       <div className="">
         <div className="center-horizontally page-header">
-          <div className="col-md-4 col-md-offset-4 modal-black pt-10">
+          <div className="col-md-4 col-md-offset-4 modal-black center-vertically">
             <div className="modal-content">
               <div className="modal-body ">
               <LoginForm history={this.props.history} />
